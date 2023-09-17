5ba5b236d896ca8f978df2dc056ff3481db2b60a
toum21 <pey2moo1@gmail.com>
2023-09-17 13:57:02 -0700

    created a getitem function in local storage that saves the users schdule after refresh

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -35,9 +35,20 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   function saveLocalData() {
     
+    $('#hour-9').children('.description').val(localStorage.getItem('hour-9'));
+    $('#hour-10').children('.description').val(localStorage.getItem('hour-10'));
+    $('#hour-11').children('.description').val(localStorage.getItem('hour-11'));
+    $('#hour-12').children('.description').val(localStorage.getItem('hour-12'));
+    $('#hour-13').children('.description').val(localStorage.getItem('hour-13'));
+    $('#hour-14').children('.description').val(localStorage.getItem('hour-14'));
+    $('#hour-15').children('.description').val(localStorage.getItem('hour-15'));
+    $('#hour-16').children('.description').val(localStorage.getItem('hour-16'));
+    $('#hour-17').children('.description').val(localStorage.getItem('hour-17')); 
+    
   }
   // this displays the current day and math
   $('#currentDay').text(dayjs().format('dddd, MMMM D'));
   hourChecker();
+  saveLocalData();
 });
 
